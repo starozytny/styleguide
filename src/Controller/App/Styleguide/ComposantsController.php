03b69041df65e77b15e72f20bf2d69a7d556df00
Shanbora <chanbora@logilink.fr>
2023-02-13 15:39:34 +0100

    init card style

--- src/Controller/App/Styleguide/ComposantsController.php
+++ src/Controller/App/Styleguide/ComposantsController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/styleguide/composants', name: 'app_styleguide_components_')]
+#[Route('/styleguide/components', name: 'app_styleguide_components_')]
 class ComposantsController extends AbstractController
 {
     #[Route('/', name: 'index')]
@@ -15,7 +15,13 @@ class ComposantsController extends AbstractController
         return $this->render('app/pages/styleguide/components/index.html.twig');
     }
 
-    #[Route('/boutons', name: 'buttons')]
+    #[Route('/cards', name: 'cards')]
+    public function cards(): Response
+    {
+        return $this->render('app/pages/styleguide/components/cards.html.twig');
+    }
+
+    #[Route('/buttons', name: 'buttons')]
     public function buttons(): Response
     {
         return $this->render('app/pages/styleguide/components/buttons.html.twig');
